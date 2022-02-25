992c5ae59b432e64794d1ebfbaaeb387d4aafeaf
itsbillw <itsbillw@gmail.com>
2022-02-26 00:12:53 +0100

    heroku

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/")
-async def homepage(request):
-    return templates.TemplateResponse('index.html', {'request': request})
+def read_root():
+    return {"Hello": "World"}
